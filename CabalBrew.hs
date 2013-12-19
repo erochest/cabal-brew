467b397e18c31b5d4655d38f4329621e7322edf9
Eric Rochester <erochest@virginia.edu>
2013-12-18 21:59:12 -0500

    A little clean-up and linting.

--- CabalBrew.hs
+++ CabalBrew.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
+{-# OPTIONS_GHC -Wall #-}
 
 
 module Main where
@@ -12,7 +13,6 @@ import           Control.Error
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Writer.Strict
-import qualified Data.ByteString                       as BS
 import qualified Data.ByteString.Lazy                  as LBS
 import           Data.Conduit
 import qualified Data.DList                            as D
@@ -21,9 +21,10 @@ import           Data.Maybe
 import           Data.Text                             (Text)
 import qualified Data.Text                             as T
 import qualified Data.Text.Encoding                    as TE
-import           Data.Traversable
 import           Data.Version
-import           Distribution.Package                  hiding (PackageName)
+import           Distribution.Package                  hiding (PackageName,
+                                                        packageName,
+                                                        packageVersion)
 import           Distribution.PackageDescription
 import           Distribution.PackageDescription.Parse
 import qualified Filesystem.Path.CurrentOS             as FS
@@ -31,7 +32,7 @@ import           Network.HTTP.Conduit
 import           Network.HTTP.Conduit.Browser
 import           Options.Applicative
 import qualified Options.Applicative                   as O
-import           Prelude                               hiding (FilePath)
+import           Prelude                               hiding (FilePath, log)
 import           Shelly
 import           Text.ParserCombinators.ReadP
 
@@ -62,8 +63,8 @@ execCabalBrew = fmap fst . runCabalBrew
 
 logCabalBrew :: (Functor m, MonadIO m) => CabalBrewRun a -> m (Either String [Text])
 logCabalBrew m = do
-    (out, log) <- runCabalBrew m
-    return $ log <$ out
+    (a, w) <- runCabalBrew m
+    return $ w <$ a
 
 log :: Text -> CabalBrewRun ()
 log = liftW . tell . D.singleton
@@ -97,10 +98,11 @@ cabalBrew Install{..} = do
         rm_rf . (cellar FS.</>) $ fromText keg
 
     liftSh . chdir "/tmp" $ do
+        let packageSpec = packageName <> "-" <> version
         whenM (test_f "cabal.sandbox.config") $ rm "cabal.sandbox.config"
-        echo $ "cabal " <> packageName <> "-" <> version <> " => " <> toTextIgnore sandbox
+        echo $ "cabal " <> packageSpec <> " => " <> toTextIgnore sandbox
         cabal_ "sandbox" ["init", "--sandbox=" <> toTextIgnore sandbox]
-        cabal_ "install" ["-j", packageName <> "-" <> version]
+        cabal_ "install" ["-j", packageSpec]
         brew_ "link" ["--overwrite", keg]
 
 cabalBrew (Update []) = do
@@ -121,20 +123,16 @@ getPackageDirectory :: PackageName -> FilePath
 getPackageDirectory = FS.append cellar . fromText . T.append "cabal-"
 
 update :: PackageName -> CabalBrewRun ()
-update packageName = do
-    v0 <- getCurrentVersion packageName
-    v1 <- getHackageVersion packageName
+update pkgName = do
+    v0 <- getCurrentVersion pkgName
+    v1 <- getHackageVersion pkgName
     when (v0 < v1) $ do
         let v1' = showv v1
-        echo' $ ">>> Updating " <> packageName <> ": " <> showv v0 <> " => " <> v1'
-        cabalBrew . Install packageName $ Just v1'
+        echo' $ ">>> Updating " <> pkgName <> ": " <> showv v0 <> " => " <> v1'
+        cabalBrew . Install pkgName $ Just v1'
         echo' ""
     where showv = T.pack . showVersion
           echo' = liftSh . echo
-
-eitherError :: Either String a -> Sh a
-eitherError (Right a)  = return a
-eitherError (Left msg) = errorExit $ T.pack msg
 
 getCurrentVersion :: PackageName -> CabalBrewRun Version
 getCurrentVersion =
@@ -146,8 +144,8 @@ readVersion :: String -> Maybe Version
 readVersion = listToMaybe . map fst . filter (null . snd) . readP_to_S parseVersion
 
 getHackageVersion :: PackageName -> CabalBrewRun Version
-getHackageVersion name =
-    pkgVersion . package . packageDescription <$> getCabal name
+getHackageVersion =
+    fmap (pkgVersion . package . packageDescription) . getCabal
 
 getCabalReq :: PackageName -> IO (Request m)
 getCabalReq name =
@@ -181,6 +179,7 @@ main = execParser opts >>= void . runCabalBrew . cabalBrew . mode
                                    \ to be managed by Homebrew."
                        )
 
+installOptions :: ParserInfo CabalBrew
 installOptions = info (helper <*> opts)
                       (  fullDesc
                       <> progDesc "This installs a Haskell program\
@@ -193,6 +192,7 @@ installOptions = info (helper <*> opts)
                             <> help "The version to install."
                             )
 
+updateOptions :: ParserInfo CabalBrew
 updateOptions = info (helper <*> opts)
                      (  fullDesc
                      <> progDesc "This updates one or more Haskell programs\
